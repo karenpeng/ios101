ccb37e529ab278880f0d145ccb50cc3075e8e759
Karen <karenpenglabs@gmail.com>
2016-11-20 15:20:30 -0800

    starting on collection

---
+++ fakeflickr/CollectionViewController.swift
@@ -0,0 +1,10 @@
+//
+//  CollectionViewController.swift
+//  fakeflickr
+//
+//  Created by karenpeng on 11/19/16.
+//  Copyright © 2016 karenpeng. All rights reserved.
+//
+
+import Foundation
+import UIKit
