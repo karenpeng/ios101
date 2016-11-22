688737896d41414cfa98a75dc193178f6f074145
Karen <karenpenglabs@gmail.com>
2016-11-21 23:54:52 -0800

    draw images synchronously

--- fakeflickr/CollectionViewController.swift
+++ fakeflickr/CollectionViewController.swift
@@ -11,8 +11,18 @@ import UIKit
 
 class CollectionViewController: UICollectionViewController {
     
+    let flickrService = FlickrService()
+    var photoUrls: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        flickrService.httpGet {
+            (data, error) in
+            self.photoUrls = self.flickrService.generatePhotoUrls(data: data)
+            print(self.photoUrls)
+            
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -21,12 +31,30 @@ class CollectionViewController: UICollectionViewController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return 4
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
       cell.backgroundColor = UIColor.black
+        
+      let url = URL(string: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT49md2-g0REsg7747eYEAtdaoo7nD6zGhao5rdY8E72RtV0VFc8Q")
+        
+      do{
+        let imageData = try Data(contentsOf: url!)
+        let img = UIImage(data: imageData)
+            
+        guard let image = img else {
+            return cell
+      }
+            
+        let imgView = cell.viewWithTag(1) as! UIImageView
+        imgView.image = img
+            
+      } catch {
+        print(error)
+      }
+
       return cell
     }
 }
--- fakeflickr/ViewController.swift
+++ fakeflickr/ViewController.swift
@@ -13,30 +13,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let flickrService = FlickrService()
-        flickrService.httpGet {
-            (data, error) in
-            let photosUrls = flickrService.generatePhotoUrls(data: data)
-            let url = URL(string: photosUrls[0])
-            do {
-                let imageData = try Data(contentsOf: url!)
-                let img = UIImage(data: imageData)
-
-                guard let image = img else {
-                    return
-                }
-                
-                let imgView = UIImageView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
-                imgView.contentMode = .scaleAspectFit
-                imgView.clipsToBounds = true
-                imgView.image = img
-                self.view.addSubview(imgView)
-
-            } catch {
-                print(error)
-            }
-            
-        }
+//        let flickrService = FlickrService()
+//        flickrService.httpGet {
+//            (data, error) in
+//            let photosUrls = flickrService.generatePhotoUrls(data: data)
+//            let url = URL(string: photosUrls[0])
+//            do {
+//                let imageData = try Data(contentsOf: url!)
+//                let img = UIImage(data: imageData)
+//
+//                guard let image = img else {
+//                    return
+//                }
+//                
+//                let imgView = UIImageView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
+//                imgView.contentMode = .scaleAspectFit
+//                imgView.clipsToBounds = true
+//                imgView.image = img
+//                self.view.addSubview(imgView)
+//
+//            } catch {
+//                print(error)
+//            }
+//            
+//        }
     }
 
     override func didReceiveMemoryWarning() {
