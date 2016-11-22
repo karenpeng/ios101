ee1a9ff548d82963943c41579d683d8a0d8a01ae
Karen <karenpenglabs@gmail.com>
2016-11-22 00:18:56 -0800

    create viewlayout

--- fakeflickr/CollectionViewController.swift
+++ fakeflickr/CollectionViewController.swift
@@ -9,7 +9,10 @@
 import Foundation
 import UIKit
 
-class CollectionViewController: UICollectionViewController {
+let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+let itemsPerRow: CGFloat = 3
+
+class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     let flickrService = FlickrService()
     var photoUrls: [String] = []
@@ -31,7 +34,7 @@ class CollectionViewController: UICollectionViewController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return 14
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -56,5 +59,29 @@ class CollectionViewController: UICollectionViewController {
       }
 
       return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath:
+                        IndexPath) -> CGSize {
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    // 4
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
     }
 }
