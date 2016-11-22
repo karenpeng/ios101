803a57cd4a5e8ce24e585a0c29ffc1ca0763cb53
Karen <karenpenglabs@gmail.com>
2016-11-21 21:55:53 -0800

    add collection view controller

--- fakeflickr/CollectionViewController.swift
+++ fakeflickr/CollectionViewController.swift
@@ -8,3 +8,25 @@
 
 import Foundation
 import UIKit
+
+class CollectionViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+      let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+      cell.backgroundColor = UIColor.black
+      return cell
+    }
+}
